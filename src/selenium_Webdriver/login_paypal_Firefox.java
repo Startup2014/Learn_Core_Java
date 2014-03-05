b7652bed51a58df27ad1f018e8bbc112a14eb811
swatidhoke <swatidhoke@gmail.com>
2014-03-05 15:11:39 -0800

    clean up 2
    clean up 2

--- src/selenium_Webdriver/login_paypal_Firefox.java
+++ src/selenium_Webdriver/login_paypal_Firefox.java
@@ -16,26 +16,19 @@ public class login_paypal_Firefox {
     public static void main(String[] args) throws Exception {
         // The Firefox driver supports javascript 
         WebDriver driver = new FirefoxDriver();
-        
-        // Go to the Google Suggest home page
-        driver.get("http://www.google.com/webhp?complete=1&hl=en");
-        
-        // Enter the query string "Cheese"
+        String website = "http://www.paypal.com";   
+        driver.get(website);
         WebElement query = driver.findElement(By.name("q"));
         query.sendKeys("Cheese");
-
-        // Sleep until the div we want is visible or 5 seconds is over
         long end = System.currentTimeMillis() + 5000;
         while (System.currentTimeMillis() < end) {
             WebElement resultsDiv = driver.findElement(By.className("gssb_e"));
-
-            // If results have been returned, the results are displayed in a drop down.
             if (resultsDiv.isDisplayed()) {
               break;
             }
         }
 
-        // And now list the suggestions
+     
         List<WebElement> allSuggestions = driver.findElements(By.xpath("//td[@class='gssb_a gbqfsf']"));
         
         for (WebElement suggestion : allSuggestions) {
